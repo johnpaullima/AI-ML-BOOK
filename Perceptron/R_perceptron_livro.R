7e91ff34998e7086bfd52ad533acc84560f4a6e5
johnpaullima <45721283+johnpaullima@users.noreply.github.com>
2021-06-09 21:25:32 -0300

    Update R_perceptron_livro.R

--- Perceptron/R_perceptron_livro.R
+++ Perceptron/R_perceptron_livro.R
@@ -1,17 +1,17 @@
 #carrega dados de entrada
-dados<- read.csv("maca_perceptron.csv", sep=";")
+dados <- read.csv("maca_perceptron.csv", sep=";")
 
-#função para normalização dos dados
+#funÃ§Ã£o para normalizaÃ§Ã£o dos dados
 normalize <- function(x) {
   return ((x - min(x)) / (max(x) - min(x)))
 }
-#aplica a normalização nos dados de entrada
-dados[,1]<-normalize(dados[,1])
-dados[,2]<-normalize(dados[,2])
-#função de treinamento do perceptron
+#aplica a normalizaÃ§Ã£o nos dados de entrada
+dados[,1] <- normalize(dados[,1])
+dados[,2] <- normalize(dados[,2])
+#funÃ§Ã£o de treinamento do perceptron
 perceptron <- function(x, y, learning_r , niter) {
-  # cria vetores dos pesos e do número de erros por épocas (niter)
-  #os pesos são inicializados de maneira aleatória (runif(1))
+  # cria vetores dos pesos e do nÃºmero de erros por Ã©pocas (niter)
+  #os pesos sÃ£o inicializados de maneira aleatÃ³ria (runif(1))
   peso <- rep(runif(1), dim(x)[2] + 1)
   numamostras <- dim(x)[1]
   erros <- rep(0, niter)
@@ -24,7 +24,7 @@ perceptron <- function(x, y, learning_r , niter) {
   # varre todos os dados de treinamento
   for (ii in 1:length(y)) {
       
-    # aplica a unidade de soma e a função de ativação
+    # aplica a unidade de soma e a funÃ§Ã£o de ativaÃ§Ã£o
     z <- sum(peso[2:length(peso)] * 
                as.numeric(x[ii, ])) + peso[1]
     if(z < 0) {
@@ -33,33 +33,33 @@ perceptron <- function(x, y, learning_r , niter) {
       ypred <- 1
     }
       
-    # modifica o peso, quando há diferença entre o previsto e real
+    # modifica o peso, quando hÃ¡ diferenÃ§a entre o previsto e real
     pesodiff <- learning_r  * (y[ii] - ypred) * 
       c(1, as.numeric(x[ii, ]))
     peso <- peso + pesodiff
 
-    # Atualiza o número de amostras erradas
+    # Atualiza o nÃºmero de amostras erradas
     if ((y[ii] - ypred) != 0.0) {
       erros[jj] <- erros[jj] + 1
     }
       
   }
-   #mostra no console a época e os pesos atuais
+   #mostra no console a Ã©poca e os pesos atuais
    acuracia[jj] <- (100*(numamostras-erros[jj])/numamostras)
    print(c(jj,peso,acuracia[jj]), digits = 3)
  }
   
-  # mostra os pesos obtidos após a ultima época
+  # mostra os pesos obtidos apÃ³s a ultima Ã©poca
   print("Pesos finais")
   print(peso)
 
   return(acuracia)
 }
-#define o número máximo de épocas 
+#define o nÃºmero mÃ¡ximo de Ã©pocas 
 nepocas <- 30
 #define a taxa de aprendizagem
 tx_aprendizagem <- 0.01
 acuracia <- perceptron(dados[,1:2], dados[,3], tx_aprendizagem, nepocas)
-plot(1:nepocas, acuracia, type="l", lwd=2, col="red", xlab="épocas", ylab="acurácia",ylim=c(0,100))
-title("Acurácia vs épocas")
+plot(1:nepocas, acuracia, type="l", lwd=2, col="red", xlab="Ã©pocas", ylab="acurÃ¡cia",ylim=c(0,100))
+title("AcurÃ¡cia vs Ã©pocas")
 
