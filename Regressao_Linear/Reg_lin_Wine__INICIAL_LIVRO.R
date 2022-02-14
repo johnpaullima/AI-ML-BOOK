12395d0b50f073be052eee85cdb2537e507c741f
johnpaullima <45721283+johnpaullima@users.noreply.github.com>
2022-02-14 12:01:01 -0300

    Update Reg_lin_Wine__INICIAL_LIVRO.R

--- Regressao_Linear/Reg_lin_Wine__INICIAL_LIVRO.R
+++ Regressao_Linear/Reg_lin_Wine__INICIAL_LIVRO.R
@@ -4,24 +4,28 @@ dados <- read.csv("wine.csv", header = TRUE, sep=",")
 #plota os dados, coluna a coluna
 plot(dados)
 
-#cria o modelo de regressão linear
+#cria o modelo de regressÃ£o linear
 linearMod <- lm(Price ~ AGST, data=dados) 
 
 #apresenta os coeficientes linear (b) e angular (a)
 linearMod
 
-#apresenta as métricas do modelo (residuo, distribuição, etc)
+#apresenta as mÃ©tricas do modelo (residuo, distribuiÃ§Ã£o, etc)
 print(summary(linearMod))
 
 #remove a coluna price do dataset dados
 X1 <- subset(dados, select = -c(Price))
 
-#Aplica a variável X1 no modelo e preveja o resultado
+#Aplica a variÃ¡vel X1 no modelo e preveja o resultado
 Ytest <- predict(linearMod, X1)
 print('valor Previsto')
 print(Ytest)
 print('valor Esperado')
 print(dados$Price) 
+
+#plota os dados originais e o calculado
+plot(dados$AGST,dados$Price)
+points(dados$AGST,Ytest,col = "blue")
 
 #calcula o RMSE
 diferencas <- dados$Price-Ytest
